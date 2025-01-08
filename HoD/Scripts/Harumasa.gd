c0c759165b8cc1c0b4e40623aa47f00eb5cbefa2
Sora556 <szentmiklosi0420@gmail.com>
2025-01-08 21:24:55 +0100

    :3

--- HoD/Scripts/Harumasa.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func _on_area_entered(area):
-	print("HARUMASAAA AHHHHH💖💖💧💧💧")
---
+++ HoD/Scripts/Player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var run_speed = 200
+var jump_speed = -400
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+
+
+func _physics_process(delta):
+	velocity.y += delta * gravity
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("Run")
+		velocity.x = direction * SPEED
+		
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("Run")
+		velocity.x = direction * SPEED
+		
+	elif Input.is_action_just_released("ui_right"):
+		$AnimatedSprite2D.play("Idle")
+		velocity.x = 0
+	elif Input.is_action_just_released("ui_left"):
+		$AnimatedSprite2D.play("Idle")
+		velocity.x = 0
+	move_and_slide()
