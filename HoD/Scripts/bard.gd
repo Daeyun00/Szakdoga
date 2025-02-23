d7fd43117cea0d26b8eff39111489c3994d770d4
szecsuani (claudia) <timarklaudia2005@gmail.com>
2025-02-23 15:24:10 +0100

    All NPC interactions

---
+++ HoD/Scripts/bard.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var is_player_within_area: bool
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		is_player_within_area = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		is_player_within_area = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_player_within_area:
+		Dialogic.start("musician")
