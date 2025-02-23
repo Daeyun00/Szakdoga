f0201df539a741342a06316e17c09e74b41052aa
szecsuani (claudia) <timarklaudia2005@gmail.com>
2025-02-23 11:13:01 +0100

    FINALLY working dialogic plugin + made 3 npc interactions

--- HoD/Scenes/World scenes/AtIntroScene/intarea.gd
+++ HoD/Scenes/World scenes/AtIntroScene/intarea.gd
@@ -1,26 +1,15 @@
 extends Area2D
 
-var player_is_in_area = false
-# Called when the node enters the scene tree for the first time.
+var is_player_within_area: bool
 
-func _process(delta: float) -> void:
-	if player_is_in_area:
-		get_tree().paused
-		if Input.is_action_pressed("interact"):
-			run_dialogue("1")
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		is_player_within_area = true
 
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		is_player_within_area = false
 
-
-
-func run_dialogue(dialogue_string):
-	Dialogic.start(dialogue_string)
-
-
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	if body.has_method("Player"):
-		player_is_in_area = true
-
-
-func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-		if body.has_method("Player"):
-			player_is_in_area = false
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_player_within_area:
+		Dialogic.start("crystal")
--- HoD/Scripts/butcher_NPCC.gd
+++ HoD/Scripts/butcher_NPCC.gd
@@ -1,30 +1,15 @@
 extends Area2D
 
-
-
-var player_in_area = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		run_dialogue("meatvendor")
-
-
-func run_dialogue(dialogue_string):
-	Dialogic.start(dialogue_string)
-
-
-
+var is_player_within_area: bool
 
 func _on_body_entered(body: Node2D) -> void:
-		if body.has_method("Player"):
-			player_in_area = true
-
+	if body.name == "Player":
+		is_player_within_area = true
 
 func _on_body_exited(body: Node2D) -> void:
-		if body.has_method("Player"):
-			player_in_area = false
+	if body.name == "Player":
+		is_player_within_area = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_player_within_area:
+		Dialogic.start("meatvendor")
