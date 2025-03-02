c3cee061549cd72d62f903d46a2e19244625df07
Gergo222 <gergoegyed7@gmail.com>
2025-03-02 17:51:56 +0100

    adatbázi táblák

--- HoD/Scripts/SQLController.gd
+++ HoD/Scripts/SQLController.gd
@@ -4,50 +4,67 @@ var database : SQLite
 
 func _ready():
 	database = SQLite.new()
-	database.path = "res://data.db"
+	database.path = "res://database.db"
 	database.open_db()
 
 
-func _on_insertdata_button_down() -> void: #adat adása 
-	var data = {
-		"name": $Name.text,
-		"score": int($Score.text)
-	}
-	database.insert_row("players", data)
 
-
-func _on_selectdata_button_down() -> void: #select metod 3 cuccli 1. tábla 2. feltétel 3. mit adjon vissza
-	print(database.select_rows("players", "score > 10", ["*"]))
-
-
-func _on_updatedata_button_down() -> void:
-	database.update_rows("players", "name = '" + $Name.text + "'", {"score": int($Score.text), "name":"Mark"})
-
-
-func _on_deletedata_button_down() -> void:
-	database.delete_rows("players", "name = '" + $Name.text + "'")
-
-
-func _on_custom_select_button_down() -> void: #sql lekérdezés
-	database.query("select * from players join playerInfo on playerInfo.id = players.playerinfoid where score > " + $Score.text)
-	for i in database.query_result:
-		print(i)
 
 
 func _on_create_table_button_down() -> void:
-	var table = {
+	var alliesTable = {
 		"id": {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment": true},
 		"name":{"data_type":"text"},
-		"score":{"data_type":"int"}
+		"lvl":{"data_type":"int"},
+		"exp":{"data_type":"int"},
+		"gold":{"data_type":"int"},
+		"atk":{"data_type":"int"},
+		"max_att":{"data_type":"int"},
+		"def":{"data_type":"int"},
+		"max_def":{"data_type":"int"},
+		"hp":{"data_type":"int"},
+		"max_hp":{"data_type":"int"},
+		"resource":{"data_type":"int"},
+		"max_resource":{"data_type":"int"},
+		"resource_name":{"data_type":"int"}
+		
+		
 	}
-	database.create_table("players", table)
-
-
-func _on_store_player_image_button_down() -> void:
-	var image := preload("res://Assets/Wooden UI/Items/Stamina Potion.png")
-	var pba = image.get_image().save_png_to_buffer()
-	database.update_rows("players", "name = 'laura'", {"picture" : pba})
-
-
-
+	var hostilesTable = {
+		"id": {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment": true},
+		"name":{"data_type":"text"},
+		"atk":{"data_type":"int"},
+		"max_att":{"data_type":"int"},
+		"def":{"data_type":"int"},
+		"max_def":{"data_type":"int"},
+		"hp":{"data_type":"int"},
+		"max_hp":{"data_type":"int"},
+		"loot_gold":{"data_type":"int"},
+		"loot_exp":{"data_type":"int"},
+		"loot_items":{"data_type":"int"}
+	}
+	var itemTable = {
+		"id": {"data_type":"int", "primary_key": true, "not_null": true, "auto_increment": true},
+		"name":{"data_type":"text"}
+	}
+	var shopInventoryTable = {
+		"Item_id":{"data_type": "int"},
+		"cost":{"data_type": "int"},
+		"bought":{"data_type":"bool"}
+	}
+	var inventoryTable = {
+		"id":{"data_type": "int", "auto_increment": true},
+		"item_id":{"data_type": "int"}
+	}
+	var mapBattlesTable = {
+		"id":{"data_type": "int", "auto_increment": true},
+		"enemy": {"data_type": "int"},
+		"quantity": {"data_type":"int"}
+	}
+	database.create_table("allies", alliesTable)
+	database.create_table("hostiles", hostilesTable)
+	database.create_table("items", itemTable)
+	database.create_table("shopInventory", shopInventoryTable)
+	database.create_table("inventory", inventoryTable)
+	database.create_table("map_battle", mapBattlesTable)
 	
