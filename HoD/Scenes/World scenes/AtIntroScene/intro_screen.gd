e773c36a26b2c1f26a06560525618dd550ccd915
szecsuani (claudia) <timarklaudia2005@gmail.com>
2025-03-06 11:14:00 +0100

    fixed stopping while dialogic running + added tutorial texts

--- HoD/Scenes/World scenes/AtIntroScene/intro_screen.gd
+++ HoD/Scenes/World scenes/AtIntroScene/intro_screen.gd
@@ -4,7 +4,11 @@ extends Node2D
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	
+	Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
 	Ui.menu.hide()
+	 
+
+
+
+func _on_animation_player_animation_started(anim_name: StringName) -> void:
 	Dialogic.start("intro")
-	
-	
