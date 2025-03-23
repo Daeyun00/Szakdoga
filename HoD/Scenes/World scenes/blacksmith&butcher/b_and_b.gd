a8dfd10644a04dab498020bafaf8fef4e300748f
Sora556 <szentmiklosi0420@gmail.com>
2025-03-23 13:16:56 +0100

    jezus

--- HoD/Scenes/World scenes/blacksmith&butcher/b_and_b.gd
+++ HoD/Scenes/World scenes/blacksmith&butcher/b_and_b.gd
@@ -1,10 +1,16 @@
 extends Node2D
 @onready var inventory = $CanvasLayer/Player/InventoryUI2
-
+@onready var labello = get_node("%Balance")
 var gold = 100
 
+func _physics_process(delta: float) -> void:
+	pass
+	
 func _ready():
-	%Balance.text = "100"
+	if (labello != null):
+		labello.set_text("100")
+	else:
+		print("Label not found!")
 
 func _input(event):
 	if event.is_action_pressed("ui_inventory"):
