1980732c75dc9e8a9e2e7aafa447d6f8239dfe49
Sora556 <szentmiklosi0420@gmail.com>
2025-03-24 19:01:05 +0100

    ?

---
+++ HoD/Scenes/World scenes/VillageCenter/teleport_next.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var teleport_ready = false
+
+
+func _process(delta: float) -> void:
+	if(teleport_ready && Input.is_action_pressed("ui_up")):
+		get_tree().change_scene_to_file("res://Scenes/World scenes/Weird guy scene/weird_guy.tscn")
+
+func _on_area_entered(area: Area2D) -> void:
+	teleport_ready = true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	teleport_ready = false
