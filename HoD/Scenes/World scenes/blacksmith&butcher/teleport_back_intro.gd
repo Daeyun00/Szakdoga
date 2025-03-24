23b9a2fe29a02e9bb8fe646bec89cee7954baf40
Sora556 <szentmiklosi0420@gmail.com>
2025-03-24 10:07:47 +0100

    ASS

---
+++ HoD/Scenes/World scenes/blacksmith&butcher/teleport_back_intro.gd
@@ -0,0 +1,14 @@
+extends Area2D
+var teleport_ready = false
+
+func _process(delta: float) -> void:
+	if(teleport_ready and Input.is_action_pressed("ui_up")):
+		get_tree().change_scene_to_file("res://Scenes/World scenes/AtIntroScene/atintroscene_.tscn")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	teleport_ready = true
+
+
+func _on_area_exited(area: Area2D) -> void:
+	teleport_ready = false
