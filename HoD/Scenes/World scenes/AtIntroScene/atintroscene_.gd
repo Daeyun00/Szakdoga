4ddec7d97f2891fb1a3347069a95fa9dd6c0108b
Sora556 <szentmiklosi0420@gmail.com>
2025-03-24 09:29:05 +0100

    teleport_intro_to_butcher

--- HoD/Scenes/World scenes/AtIntroScene/atintroscene_.gd
+++ HoD/Scenes/World scenes/AtIntroScene/atintroscene_.gd
@@ -1,1 +1,13 @@
 extends Node2D
+var teleport_ready = false
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	teleport_ready = true
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	teleport_ready = false
+
+func _process(delta: float) -> void:
+	if(teleport_ready):
+		if(Input.is_action_pressed("ui_up")):
+			get_tree().change_scene_to_file("res://Scenes/World scenes/blacksmith&butcher/blacksmith_and_butcher.tscn")
