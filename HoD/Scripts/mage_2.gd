4dc13bd70fcac1f7c71c72f9cbe072e1ba7df67f
szecsuani (claudia) <timarklaudia2005@gmail.com>
2025-03-25 09:45:11 +0100

    portal to dungeon + fixes

--- HoD/Scripts/mage_2.gd
+++ HoD/Scripts/mage_2.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
-
+@onready var mage_2: Area2D = $"."
+var first_interaction : bool
 var is_player_within_area: bool
+
+
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
@@ -12,5 +17,13 @@ func _on_body_exited(body: Node2D) -> void:
 		is_player_within_area = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") and is_player_within_area:
+	if event.is_action_pressed("interact") and is_player_within_area and first_interaction == false:
 		Dialogic.start("mage")
+		first_interaction = true
+		
+	if event.is_action_pressed("interact") and is_player_within_area and first_interaction == true:
+		Dialogic.end_timeline()
+		
+func _on_dialogic_signal(argument:String):
+	if argument == "mage_joined":
+		mage_2.hide()
